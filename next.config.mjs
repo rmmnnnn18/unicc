3f9ec2622da4b65305920e1fdc4235b9594ddeae
rmmnnnn18 <rmmnnnn.18@gmail.com>
2025-09-19 11:28:58 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import withSerwistInit from "@serwist/next";
+
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+const revision = crypto.randomUUID();
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: "src/app/sw.js",
+  swDest: "public/sw.js",
+  additionalPrecacheEntries: [{ url: "/~offline", revision }],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+export default withSerwist(nextConfig);
